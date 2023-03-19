15a53a297dccc24754d13e2fd2fcf79fac8df80e
Young-Joon Park <yopark@sas.upenn.edu>
2023-03-18 20:05:18 -0700

    Apply details page outline

--- lib/entry_lists.dart
+++ lib/entry_lists.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/intl.dart';
+
 
 class EntryLists extends StatefulWidget {
   const EntryLists({Key? key}) : super(key: key);
@@ -15,7 +18,7 @@ class EntryListsState extends State<EntryLists> {
       appBar: AppBar(title: const Text('Band Names')),
       body: StreamBuilder(
           stream:
-              FirebaseFirestore.instance.collection('bandnames').snapshots(),
+              FirebaseFirestore.instance.collection('bandnames').orderBy('timeStamp', descending: true).snapshots(),
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
@@ -24,8 +27,10 @@ class EntryListsState extends State<EntryLists> {
                   itemBuilder: (context, index) {
                     var post = snapshot.data!.docs[index];
                     return ListTile(
-                        title: Text(post['name']),
-                        subtitle: Text(post['votes'].toString()));
+                        title: Text(post['dateTime']),
+                        trailing: Text(post['number'].toString()),
+                        onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) =>  JournalDetails(post['name'], post['number'], post['dateTime'])));}
+                    );
                   });
             } else {
               return const Center(child: CircularProgressIndicator());
@@ -49,9 +54,49 @@ class NewEntryButton extends StatelessWidget {
     return FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
+          DateTime now = DateTime.now();
+          String formattedDate = DateFormat('kk:mm:ss EEE d MMM').format(now);
           FirebaseFirestore.instance
               .collection('bandnames')
-              .add({'name': 'Rusty Laptop', 'votes': 22});
+              .add({'name': 'Big Tinks', 'number': 22, 'dateTime' : formattedDate, 'timeStamp' : FieldValue.serverTimestamp()});
         });
   }
+}
+//
+// Details page
+//
+class JournalDetails extends StatelessWidget {
+  JournalDetails(this.title, this.body, this.date, {super.key});
+
+  String title;
+  int body;
+  String date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Entry Details'),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('\n$date\n', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold, fontSize: 28)),
+              Text('image', textAlign: TextAlign.center),
+              Text('\n$body\n'),
+              ElevatedButton(
+                onPressed: () => Navigator.pop(context), 
+                child: Text('Back to Entry List'))
+            ]
+          )
+        )
+      )
+    );
+  }
+
 }
